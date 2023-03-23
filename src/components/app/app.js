5c2d00dc280dd0c37768ee8c62ee1310401fd6ba
KovOlga <switch.to.olga@yandex.ru>
2023-03-23 17:37:06 +0300

    added components division

---
+++ src/components/app/app.js
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="app">
+      <main></main>
+    </div>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
+import ReactDOM from "react-dom";
+import App from "./components/app/app";
+import "./style/style.scss";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode></React.StrictMode>);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
