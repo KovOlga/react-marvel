be45dbf50c4cd75f8aee1b30a8dc36b80216b37e
KovOlga <switch.to.olga@yandex.ru>
2023-05-04 16:38:38 +0300

    added React.lazy in MainPage

--- src/components/pages/main-page.js
+++ src/components/pages/main-page.js
@@ -1,11 +1,17 @@
 import ErrorBoundary from "../error-boundary/error-boundary";
 import RandomChar from "../random-char/random-char";
 import CharList from "../char-list/char-list";
-import CharInfo from "../char-info/char-info";
+// import CharInfo from "../char-info/char-info";
 import styles from "./main-page.module.css";
 import vision from "../../images/vision.png";
 import mjolnir from "../../images/mjolnir.png";
 import { useState, useCallback } from "react";
+import Spinner from "../spinner/spinner";
+
+import { lazy } from "react";
+import { Suspense } from "react";
+
+const CharInfo = lazy(() => import("../char-info/char-info"));
 
 const MainPage = () => {
   const [selectedChar, setSelectedChar] = useState(null);
@@ -14,21 +20,23 @@ const MainPage = () => {
     setSelectedChar(id);
   }, []);
   return (
-    <main className={styles.char}>
-      <ErrorBoundary>
-        <RandomChar mjolnir={mjolnir} />
-      </ErrorBoundary>
+    <Suspense fallback={<Spinner />}>
+      <main className={styles.char}>
+        <ErrorBoundary>
+          <RandomChar mjolnir={mjolnir} />
+        </ErrorBoundary>
 
-      <div className={styles.char__content}>
-        <ErrorBoundary>
-          <CharList onCharSelected={onCharSelected} />
-        </ErrorBoundary>
-        <ErrorBoundary>
-          <CharInfo charId={selectedChar} />
-        </ErrorBoundary>
-      </div>
-      <img className={styles.char__bg} src={vision} alt="vision" />
-    </main>
+        <div className={styles.char__content}>
+          <ErrorBoundary>
+            <CharList onCharSelected={onCharSelected} />
+          </ErrorBoundary>
+          <ErrorBoundary>
+            <CharInfo charId={selectedChar} />
+          </ErrorBoundary>
+        </div>
+        <img className={styles.char__bg} src={vision} alt="vision" />
+      </main>
+    </Suspense>
   );
 };
 
